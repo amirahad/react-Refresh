7c5c22ebf9dea295fee569b1e0a3cffbe68ca3d7
amirahad <arahad4783@gmail.com>
2022-04-06 02:05:31 +0600

    useState Practiced

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const [name,setName] = useState('Amir')
+  const [showEvents,setShowevents] = useState(true)
+  const [events,setEvents] = useState([
+    {title: "amir is a good boy", id: 1},
+    {title: "raza is a good boy", id: 2},
+    {title: "ahad is a good boy", id: 3}
+  ])
+
+  // const handleclick = () => {
+  //   setName('Ahad')
+  //   console.log(name);
+  // }
+
+  const handleClick = (id) =>{
+    // console.log(id);
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id
+      })
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>My Name is {name}</h1>
+      <button onClick={handleclick}>Change Name</button> */}
+      {showEvents && events.map((event , index) => (
+        <div key={event.id}>
+         <h2>{index} = {event.title}</h2>
+         <button onClick = {() => handleClick(event.id)}>Delete</button>
+        </div>
+        
+      ))}
+      <div>
+        {!showEvents && <button onClick={() => setShowevents(true)}>Show</button>}
+        {showEvents && <button onClick={() => setShowevents(false)}>Hide</button>}
+      </div>
     </div>
   );
 }
